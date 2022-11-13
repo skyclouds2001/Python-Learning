ea30ea6d4827bda110bbbd93b8b2efb22d0aca26
skyclouds2001 <skyclouds2001@163.com>
2022-11-13 12:39:20 +0800

    feat: work-20221112

---
+++ school-work/RegainSecret-20221112.py
@@ -0,0 +1,117 @@
+import random
+from typing import TextIO
+
+
+def gcd(a: int, b: int) -> int:
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+def judge_legal_d(m: list[int], num: int) -> bool:
+    flag = True
+    for i in range(0, num):
+        for j in range(0, num):
+            if i != j and gcd(m[i], m[j]) != 1:
+                flag = False
+                break
+    return flag and (num == 1 or m[num - 2] < m[num - 1])
+
+
+def create_d_array(n: int) -> list[int]:
+    d = [1 for _ in range(0, n)]
+    temp = random.randint(pow(10, 167), pow(10, 168))
+    d[0] = temp
+    i = 1
+    while i < n:
+        temp = random.randint(pow(10, 167), pow(10, 168))
+        d[i] = temp
+        if judge_legal_d(d, i + 1):
+            i = i + 1
+    return d
+
+
+def get_n_and_m(d: list[int], t: int) -> (int, int):
+    n = 1
+    m = 1
+    for i in range(0, t):
+        n = n * d[i]
+    for i in range(len(d) - t + 1, len(d)):
+        m = m * d[i]
+    return n, m
+
+
+def get_kk(d: list[int], k: int) -> list[int]:
+    k1 = [1 for _ in range(0, len(d))]
+    for i in range(0, len(d)):
+        k1[i] = k % d[i]
+    k1 = k1[0:len(d)]
+    return k1
+
+
+def get_mod_reverse(a: int, m: int) -> int | None:
+    if gcd(a, m) != 1:
+        return None
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+
+
+def chinese_surplus(k: list[int], d: list[int], t: int) -> int:
+    m = d[0:t]
+    a = k[0:t]
+    m1 = 1
+    for i in range(0, len(m)):
+        m1 = m1 * m[i]
+    Mj = div_result(m)
+    Mj1 = [0 for _ in range(0, len(m))]
+    for i in range(0, len(m)):
+        Mj1[i] = get_mod_reverse(Mj[i], m[i])
+    x = 0
+    for i in range(0, len(m)):
+        x = x + Mj[i] * Mj1[i] * a[i]
+    result = x % m1
+    return result
+
+
+def div_result(m: list[int]) -> list[int]:
+    Mj = [1 for _ in range(0, len(m))]
+    for i in range(0, len(m)):
+        for j in range(0, len(m)):
+            if i == j:
+                Mj[i] = Mj[i] * 1
+            else:
+                Mj[i] = Mj[i] * m[j]
+    return Mj
+
+
+def main() -> None:
+    n: int = random.randint(1, 6)
+    t: int = random.randint(1, n)
+    print("n: ", n)
+    print("t: ", t)
+
+    file: TextIO = open("D:\\学习\\3·1信息安全基础与密码学综合实验\\secret1.txt")
+    key: int = int(file.readline())
+    print("key: ", key)
+
+    d: list[int] = create_d_array(n)
+    print("d: ", d)
+
+    N, M = get_n_and_m(d, t)
+    print("n: ", N)
+    print("m: ", M)
+
+    kk: list[int] = get_kk(d, key)
+    print("keys: ", kk)
+
+    res: int = chinese_surplus(kk, d, t)
+    print("recover key with t: ", res)
+
+
+if __name__ == '__main__':
+    main()
